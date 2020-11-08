d4016a83e0f3b43e80c22f4b87f69dcf40fc5b03
IslomSobirov <atomm262@gmail.com>
2020-11-08 20:43:15 +0500

    user create

--- src/dbexample.c
+++ src/dbexample.c
@@ -1,9 +1,11 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
-#include "db/db.h"
+// #include "db/db.h"
+#include "model/user/User.h"
 
 int main() {
-    connect();
+    // connect();
     // gcc -o dbexample $(mysql_config --cflags) dbexample.c  $(mysql_config --libs)
+    createUser("Islom", "12345654", "998998403675");
     return 0;
 }
--- src/model/user/User.h
+++ src/model/user/User.h
@@ -4,7 +4,7 @@
 1.  Gets inputs and store/write them.
 2.  Returns stored data .
 */
-
+#include "../../db/db.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -24,6 +24,42 @@ char setFirstName(char firstName){
     for (int i = 0; i < n; ++i)
         putchar(str[i]);
     putchar('\n'); /* trailing '\n' needed in Standard C */
+}
+
+void createUser(char name[30], char password[100], char phoneNumber[10]) {
+    connect();
+    char comma[] = ",";
+    char close[] = ")";
+    char sss[] = "'";
+    char timenow[] = "now()";
+    char sql[500] = "insert into users (name, password, phone_number, date_created) values (";
+    //very bad way of inserting to db, i will try to find other way
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, name, strlen(name));
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, comma, strlen(comma));
+
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, password, strlen(password));
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, comma, strlen(comma));
+
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, phoneNumber, strlen(phoneNumber));
+    strncat(sql, sss, strlen(sss));
+    strncat(sql, comma, strlen(comma));
+    strncat(sql, timenow, strlen(timenow));
+
+    strncat(sql, close, strlen(close));
+    printf("Sql is: %s\n", sql);
+
+
+    if (!mysql_query(conn, sql)) {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
+    mysql_close(conn);
 }
 
 int user() {
